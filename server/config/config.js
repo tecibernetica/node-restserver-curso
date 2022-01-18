808413cc996c4cfd8169eb00469c64b8a468ff13
Julio <tecibernetica@gmail.com>
2022-01-18 15:55:22 -0500

    Variable de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,8 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI+'&w=majority';
+    urlDB = process.env.MONGO_URI;
+
 }
 
 
