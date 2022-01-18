ad618862efef3a1f396f098a3dbc8eb9c181298c
Julio <tecibernetica@gmail.com>
2022-01-18 15:56:20 -0500

    Variable de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,10 +11,9 @@ process.env.PORT = process.env.PORT  || 3000;
 let urlDB;
 
 if( process.env.NODE_ENV === 'dev'){
-    urlDB = 'mongodb://localhost:27017/cafe';
+    urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = process.env.MONGO_URI;
-
+    urlDB = 'mongodb+srv://jccabrera:julio1989@cluster0.wutyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
 
 
